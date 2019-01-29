4f3a4b604583163ecd55f333ba80ec448e2e9357
DarekSkiba <dskiba@it-serwis.pl>
2019-01-29 19:36:08 +0100

    6.1. Utworzenie modułu kodilla-testing

---
+++ kodilla-testing/src/test/java/ForumTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.forum
+
+public class SimpleUser {
+    String username;
+    public SimpleUser(String username){
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
