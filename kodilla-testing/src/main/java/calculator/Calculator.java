c7a3b44cef0152ba371454fcc3147c27b65c665c
DarekSkiba <dskiba@it-serwis.pl>
2019-01-29 19:20:54 +0100

    6.1. Utworzenie modułu kodilla-testing

---
+++ kodilla-testing/src/main/java/calculator/Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator
+{
+    public static void main(String args[])
+    {
+        float a, b, res;
+        char choice, ch;
+        Scanner scan = new Scanner(System.in);
+
+        do
+        {
+            System.out.print("1. Addition\n");
+            System.out.print("2. Subtraction\n");
+            System.out.print("9. Exit\n\n");
+            System.out.print("Enter Your Choice : ");
+            choice = scan.next().charAt(0);
+            switch(choice)
+            {
+                case '1' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a + b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '2' : System.out.print("Enter Two Number : ");
+                    a = scan.nextFloat();
+                    b = scan.nextFloat();
+                    res = a - b;
+                    System.out.print("Result = " + res);
+                    break;
+                case '9' : System.exit(0);
+                    break;
+                default : System.out.print("Wrong Choice!!!");
+                    break;
+            }
+            System.out.print("\n---------------------------------------\n");
+        }while(choice != 5);
+    }
+}
