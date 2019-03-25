a6d2e6a116882f3e50d52547f3571dd9e4652587
DarekSkiba <dskiba@it-serwis.pl>
2019-03-25 22:33:52 +0100

    8.2 NO i OK

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,14 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge test = new SecondChallenge();
+
+        try {
+            test.probablyIWillThrowException(1,1);
+        } catch (Exception e) {
+            System.out.println("NO");
+        }
+    }
+
 }
